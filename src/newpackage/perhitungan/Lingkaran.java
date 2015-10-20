e55d011ff925101f83778c05844316d9e24407bb
Sinta Sagita Agustina <sintasagita16@gmail.com>
2015-10-20 23:23:31 +0700

    versi 1.0.0

---
+++ src/newpackage/perhitungan/Lingkaran.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage.perhitungan;
+
+/**
+ *
+ * @author userr
+ */
+public class Lingkaran {
+    
+        double luas;
+        public double luaslingkaran(double jari2){
+        luas = 3.14 * jari2 * jari2;
+        return luas;
+        }
+
+}
+ 
+
---
+++ src/newpackage/perhitungan/Segitiga.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage.perhitungan;
+
+/**
+ *
+ * @author userr
+ */
+public class Segitiga {
+    
+    double luas;
+    public double luassegitiga(double alas, double tinggi){
+        luas = 0.5 * alas * tinggi;
+        return luas;
+    }
+    
+}
